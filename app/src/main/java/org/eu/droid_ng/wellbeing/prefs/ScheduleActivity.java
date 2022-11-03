69d150bd0fb0f04753ad47ee2dec26373df5a1c5
nift4 <nift4@protonmail.com>
2022-11-03 22:07:51 +0100

    some schedule stuff

--- app/src/main/java/org/eu/droid_ng/wellbeing/prefs/ScheduleActivity.java
+++ app/src/main/java/org/eu/droid_ng/wellbeing/prefs/ScheduleActivity.java
@@ -3,10 +3,13 @@ package org.eu.droid_ng.wellbeing.prefs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 import org.eu.droid_ng.wellbeing.R;
 
 public class ScheduleActivity extends AppCompatActivity {
+
+	RadioButton disable, sched, schedc;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,18 @@ public class ScheduleActivity extends AppCompatActivity {
 		}
 		String type = getIntent().getStringExtra("type");
 		setContentView(R.layout.activity_schedule);
+		disable = findViewById(R.id.radioButtonDisable);
+		sched = findViewById(R.id.radioSchedule);
+		schedc = findViewById(R.id.radioCharging);
+		findViewById(R.id.layoutDisable).setOnClickListener(v -> setChecked(0));
+		findViewById(R.id.layoutSchedule).setOnClickListener(v -> setChecked(1));
+		findViewById(R.id.layoutCharging).setOnClickListener(v -> setChecked(2));
 		//TODO: implement schedules
+	}
+
+	private void setChecked(int c) {
+		disable.setChecked(c == 0);
+		sched.setChecked(c == 1);
+		schedc.setChecked(c == 2);
 	}
 }
